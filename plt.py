5a958376a078d116239b9742163622afa44a75bc
chenjfeng <993813999@qq.com>
2022-04-03 08:59:18 +0800

    常规更新

--- plt.py
+++ plt.py
@@ -1,11 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
-x=np.arange(0,6,0.1)
-y=np.sin(x)
-y1=np.cos(x)
-plt.plot(x,y,label='sin')
-plt.plot(x,y1,linestyle='--',label='cos')
-plt.title('sin &cos')
-plt.legend()
+from matplotlib.image import imread
+img=imread('img/015.jpg')
+plt.imshow(img)
 plt.show()
+
